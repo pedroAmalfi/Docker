4d4a9882f6217e3ef6cb9b5c902e08a9c0e97098
pedroAmalfi <146018730+pedroAmalfi@users.noreply.github.com>
2026-06-01 11:21:09 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ import json
 import pandas as pd
 
 from metrics import executar_medicao
-from graph_generator import gerar_grafico
+from graph_generator import gerar_graficos
 
 resultados = []
 
-numeros_sorteados = [random.randint(1, 100) for _ in range(10)]
+numeros_sorteados = [random.randint(100, 1000) for _ in range(10)]
+
+print("\nNúmeros sorteados:")
+print(numeros_sorteados)
 
 for numero in numeros_sorteados:
 
@@ -20,6 +23,7 @@ df = pd.DataFrame(resultados)
 df.to_csv("/app/output/metrics.csv", index=False)
 
 resumo = {
+    "numeros_sorteados": numeros_sorteados,
     "tempo_medio_ms": round(df["tempo_ms"].mean(), 4),
     "memoria_media_mb": round(df["memoria_mb"].mean(), 4),
     "iteracoes_media": round(df["iteracoes"].mean(), 2)
@@ -28,6 +32,15 @@ resumo = {
 with open("/app/output/metrics.json", "w") as arquivo:
     json.dump(resumo, arquivo, indent=4)
 
-gerar_grafico(df)
+print("\nResultados obtidos:")
+print(df)
 
-print("Execução concluída.")
+gerar_graficos(df)
+
+print("\nArquivos gerados em /output")
+print("- metrics.csv")
+print("- metrics.json")
+print("- performance_graph.png")
+print("- iterations_graph.png")
+
+print("\nExecução concluída.")
